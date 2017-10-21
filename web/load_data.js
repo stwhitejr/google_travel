c24eda7996f790ee5788628e9a0587b9e92f69eb
steve white <steve white>
2017-10-21 16:37:29 -0400

    Breaking out longest and shortest

--- web/load_data.js
+++ web/load_data.js
@@ -1,29 +1,61 @@
 // TODO
-// Add toggles for set times only
 // Add accordian tabs
-// Better comparison window, only 1 result of control and fix it's position
+// Get shortest and longest commute and compare to control's shortest and longest
 
 function loadData() {
+  loadJson('am');
+  loadJson('pm');
+}
+
+function loadJson(prefix) {
+  var jsonFile = prefix + '_data.json';
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-     jsonData = this.responseText;
-     loadMustache(jsonData, true);
-     xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-       jsonData = this.responseText;
-       loadMustache(jsonData, false);
-      }
-    };
-    xhttp.open("GET", "pm_data.json", true);
-    xhttp.send();
+     jsonData = JSON.parse(this.responseText);
+     // Add shortest and longest durations to recording_times
+     for (var i = 0; jsonData.length > i; i++) {
+       const recordingTimes = jsonData[i].recording_times;
+       for (var x = 0; recordingTimes.length > x; x++) {
+        // Collect all the duration_in_traffic values into one array
+         let recordingsDuration = [];
+         for (var d = 0; recordingTimes[x].recordings.length > d; d++) {
+          let currentDurationInTraffic = recordingTimes[x].recordings[d].duration_in_traffic;
+          currentDurationInTraffic = currentDurationInTraffic.replace(' mins', '');
+          if (currentDurationInTraffic.indexOf('hour') !== -1) {
+            currentDurationInTrafficHour = currentDurationInTraffic.substr(0, 1);
+            currentDurationInTraffic = currentDurationInTraffic.substr(6);
+            currentDurationInTraffic = parseInt(currentDurationInTraffic) + (parseInt(currentDurationInTrafficHour) * 60);
+          }
+          recordingsDuration.push(currentDurationInTraffic);
+         }
+         // Compare all results for shortest and longest
+         let shortestDuration = recordingsDuration[0];
+         let longestDuration = recordingsDuration[0];
+         for (var d = 1; recordingsDuration.length > d; d++) {
+          if (longestDuration < recordingsDuration[d]) {
+            longestDuration = recordingsDuration[d];
+          }
+          if (shortestDuration > recordingsDuration[d]) {
+            shortestDuration = recordingsDuration[d];
+          }
+         }
+         jsonData[i].recording_times[x].shortest = shortestDuration;
+         jsonData[i].recording_times[x].longest = longestDuration;
+         // Compare to first/control
+         jsonData[i].recording_times[x].longest_diff = longestDuration - jsonData[0].recording_times[x].longest;
+         jsonData[i].recording_times[x].shortest_diff = shortestDuration - jsonData[0].recording_times[x].shortest;
+       }
+     }
+     jsonData = JSON.stringify(jsonData);
+     loadMustache(jsonData, prefix);
     }
   };
-  xhttp.open("GET", "am_data.json", true);
+  xhttp.open("GET", jsonFile, true);
   xhttp.send();
 }
 
-function loadMustache(jsonData, isAm) {
+function loadMustache(jsonData, prefix) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -31,11 +63,7 @@ function loadMustache(jsonData, isAm) {
       jsonData = {
         locations: JSON.parse(jsonData)
       };
-      if (isAm) {
-        contentId = 'am_content';
-      } else {
-        contentId = 'pm_content';
-      }
+      contentId = prefix + '_content';
       Mustache.parse(mustacheTemplate);
       var rendered = Mustache.render(mustacheTemplate, jsonData);
       var content = document.getElementById(contentId);
